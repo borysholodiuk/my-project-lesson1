8ec56f1bf849c51215c0ac773502d737ec0f2867
karkarich102 <max.marinyak12@gmail.com>
2023-03-05 12:40:45 +0200

    Клас

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print("Hello World")
+class Student:
 
-print(1)
+    def __init__(self, name, surname, age):
+        self.name = name
+        self.surname = surname
+        self.age = age
+
+a1 = Student("Olga", "Hamota", 24)
+print(a1.name, a1.surname)
