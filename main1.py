873fc527dd9bbec4ee4f2c6502c03c5314d64d66
karkarich102 <max.marinyak12@gmail.com>
2023-03-12 12:49:53 +0200

    info 2 student

--- main1.py
+++ main1.py
@@ -1,11 +1,96 @@
+# class human:
+#     def __init__(self, name = "No name"):
+#         self.name = name
+# class Auto:
+#     def __init__(self, model):
+#         self.model = model
+#         self.pasengers = []
+#
+#     def add_pasengers(self, human):
+#         self.pasengers.append(human)
+#
+#     def car_detalis(self):
+#         if self.pasengers != []:
+#             print(f"B машині {self.model} є такі пасажири:\n{self.pasengers}")
+#             for pasengers in self.pasengers:
+#                 print(pasengers.name)
+#
+#
+# car = Auto("Audi")
+# car.add_pasengers(human("Olga"))
+# car.add_pasengers(human("Borys"))
+# car.car_detalis()
+
+
+
+
+
+
+
+
+
+# class human:
+#     def __init__(self, name = "No name"):
+#         self.name = name
+# class Auto:
+#     def __init__(self, model):
+#         self.model = model
+#         self.pasengers = []
+#
+#     def add_pasengers(self, human):
+#         self.pasengers.append(human)
+#
+#     def car_detalis(self):
+#         if self.pasengers != []:
+#             print(f"B машині {self.model} є такі пасажири:\n{self.pasengers}")
+#             for pasengers in self.pasengers:
+#                 print(pasengers.name)
+#
+#
+# car = Auto("BMV")
+# car.add_pasengers(human("Bober"))
+# car.add_pasengers(human("Capibara"))
+# car.car_detalis()
+
+
+
+
+# class human:
+#      def __init__(self, name = "No name"):
+#          self.name = name
+# class Auto:
+#      def __init__(self, model):
+#          self.model = model
+#          self.pasengers = []
+#
+#      def add_pasengers(self, human):
+#          self.pasengers.append(human)
+#
+#      def car_detalis(self):
+#          if self.pasengers != []:
+#              print(f"B автобусі {self.model} є такі пасажири:\n{self.pasengers}")
+#              for pasengers in self.pasengers:
+#                  print(pasengers.name)
+#
+#
+# car = Auto("Autosan")
+# car.add_pasengers(human("Abob"))
+# car.add_pasengers(human("tutu"))
+# car.car_detalis()
+
+
+
+
 class human:
-    def __init__(self, name = "No name", surname = "No surname" ):
+
+    def __init__(self, name="No name", surname="No surname", age= "No age"):
         self.name = name
         self.surname = surname
-class Auto:
-    def __init__(self, model):
-        self.model = model
-        self.pasengers = []
+        self.age = age
 
-    def add_pasengers(self, human):
-        self.pasengers.append(human)
+class student:
+    def __init__(self, human, Stupendia, name1):
+        self.human = human
+        self.Stupendia = Stupendia
+        self.name1 = name1
+
